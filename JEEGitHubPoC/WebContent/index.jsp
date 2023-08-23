b8f4cf8c1e339d9db5c62520e2015f73e97a58ad
ajoseph <ajoseph@ilsos.gov>
2023-08-23 13:05:44 -0500

    First Commit

---
+++ JEEGitHubPoC/WebContent/index.jsp
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML><%@page language="java"
+	contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<html>
+<head>
+<title>index</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+</head>
+<body>
+	<h1>JEE project using GitHub - PoC</h1>
+
+	<form action="testServlet" method="post">
+
+		<table>
+			<tbody>
+				<tr>
+					<td>Please enter your Name:</td>
+					<td style="width: 70%"><input type="text" name="yourName"
+						size="20"></td>
+				</tr>
+				<tr>
+					<td colspan="2"><input type="submit" value="Submit" /></td>
+				</tr>
+			</tbody>
+		</table>
+
+	</form>
+</body>
+</html>
---
+++ JEEGitHubPoC/src/aj/test/TestServlet.java
@@ -0,0 +1,38 @@
+package aj.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/testServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String yourName = request.getParameter("yourName");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hi " + yourName + "</h1>");
+		writer.close();
+	}
+
+}
